c0c670027516d5ab6a75b706c660200a33295cd1
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-12-03 12:52:04 +0900

    add: 勝敗をつけるようにした

--- lib/screen/game_screen/game_screen_controller.dart
+++ lib/screen/game_screen/game_screen_controller.dart
@@ -3,20 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class GameScreenController extends GetxController {
-  var contentAmount = 0.obs;
-
+  var contentAmount = 5.obs;
   GlobalKey<ExpandableBottomSheetState> key = GlobalKey();
   ExpansionStatus expansionStatus = ExpansionStatus.contracted;
 
   void onTapPlus() {
-    if (contentAmount < 10) contentAmount++;
+    if (contentAmount < 10) {
+      contentAmount++;
+    } else {
+      print('上の勝ち');
+    }
     print(contentAmount.value);
   }
 
   void onTapMinus() {
-    if (contentAmount > 0) contentAmount--;
+    if (contentAmount > 0) {
+      contentAmount--;
+    } else {
+      print('下の勝ち');
+    }
     print(contentAmount.value);
   }
 
-  void onTap() {}
+  void check() {
+
+  }
 }
